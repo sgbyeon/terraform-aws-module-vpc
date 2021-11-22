5aebdb494c7ad42fc917fa63f665e4dc70a0ed83
sgbyeon <sg.byeon1@gmail.com>
2021-11-22 18:10:21 +0900

    [bsg] update vpc.tf

--- locals.tf
+++ locals.tf
@@ -8,3 +8,14 @@ locals {
     ]
   ])
 }
+
+locals {
+  nat_gateway = flatten([
+    for key, value in var.subnets : [
+      for item in value.cidr : {
+        name = key
+        cidr = item
+      }
+    ] if v.ipv4_type == "public"
+  ])
+}
