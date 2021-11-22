2683dc1b8f4f7f67064e8fa0e5a434952e7e3bec
sgbyeon <sg.byeon1@gmail.com>
2021-11-22 17:45:15 +0900

    [bsg] update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -26,18 +26,16 @@ resource "aws_eip" "nat" {
 # dynamic subnet
 resource "aws_subnet" "this" {
   vpc_id = aws_vpc.this.id
-  //count = length(var.azs)
-  //for_each = { for k, v in var.subnets : k => [ for i in v.cidr : { name = k, item = i } ] }
-  for_each = { for i in local.subnets : i.cidr => i}
+  for_each = { for i in local.subnets : i.cidr => i }
   cidr_block = each.key
   availability_zone = var.azs[index(var.subnets[each.value.name].cidr, each.key)]
 
   tags = merge(var.tags, tomap({ Name = format("%s-%s-%s-%s-%s-sn", 
                                                 var.prefix,
                                                 var.vpc_name,
-                                                var.azs[index(var.subnets[each.value].cidr, each.key)],
-                                                var.subnets[each.value].ipv4_type,
-                                                each.value
+                                                var.azs[index(var.subnets[each.value.name].cidr, each.key)],
+                                                var.subnets[each.value.name].ipv4_type,
+                                                each.value.name
                                               )}))
 }
 
