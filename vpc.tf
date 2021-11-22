c2732c4c14987731fa5fd8dd1752ecdc061aac32
sgbyeon <sg.byeon1@gmail.com>
2021-11-22 16:53:08 +0900

    [bsg] update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -27,8 +27,8 @@ resource "aws_eip" "nat" {
 resource "aws_subnet" "this" {
   vpc_id = aws_vpc.this.id
   
-  for_each = flatten([ for k, v in var.subnets : [ for i in v.cidr : { name = k, item = i } ] ])
-  cidr_block = each.value.cidr
+  for_each = { for k, v in var.subnets : k => [ for i in v.cidr : { name = k, item = i } ] }
+  cidr_block = item
   availability_zone = var.azs[index(var.subnets[each.value.name].cidr, each.value.cidr)]
 
   tags = merge(var.tags, tomap({ Name = format("%s-%s-%s-%s-%s-sn", 
