93314426eeebf0eb1c24981a1e84daf202a50d10
sgbyeon <sg.byeon1@gmail.com>
2021-11-22 17:56:26 +0900

    [bsg] update vpc.tf

--- locals.tf
+++ locals.tf
@@ -3,9 +3,9 @@ locals {
     for key, value in var.subnets : [
       for item in value : {
         name = key
-        cidr = item.cidr
-        ipv4_type = item.ipv4_type
-        natgw = item.natgw
+        cidr = item[cidr]
+        ipv4_type = item[ipv4_type]
+        natgw = item[natgw]
       }
     ]
   ])
